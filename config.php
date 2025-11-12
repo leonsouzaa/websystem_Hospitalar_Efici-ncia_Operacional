fd315654da713b0fffe6b95b6d6769b3b92646d0
Leonardo Souza <leonsouza.contato@gmail.com>
2025-11-12 18:47:56 -0300

    Atualização CONFIG 1

--- config.php
+++ config.php
@@ -4,10 +4,10 @@ $DB_NAME = getenv('DB_NAME') ?: 'minimundos';
 $DB_USER = getenv('DB_USER') ?: 'root';
 $DB_PASS = getenv('DB_PASS') ?: '';
 
-$TABLE = 'coleta_seletiva';
+$TABLE = 'hospital';
 $COLUMNS = [
-  ['name' => 'bairro', 'label' => 'Bairro', 'type' => 'text'],
-  ['name' => 'tipo_residuo', 'label' => 'Tipo de Resíduo', 'type' => 'text'],
-  ['name' => 'peso_total', 'label' => 'Peso Total (kg)', 'type' => 'number', 'step' => '0.01'],
-  ['name' => 'reciclavel', 'label' => 'Peso Reciclável (kg)', 'type' => 'number', 'step' => '0.01']
+  ['name' => 'nome', 'label' => 'Hospital', 'type' => 'text'],
+  ['name' => 'leitos', 'label' => 'Nº de Leitos', 'type' => 'number'],
+  ['name' => 'atendimentos', 'label' => 'Nº de Atendimentos', 'type' => 'number'],
+  ['name' => 'obitos', 'label' => 'Nº de Óbitos', 'type' => 'number']
 ];
